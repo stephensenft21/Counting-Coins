f7d7f439ab4e68ad68847c94069b2a472e72a141
Stephen Senft <stephensenft21@gmail.com>
2019-07-30 03:54:52 -0500

    final commit

--- coins.js
+++ coins.js
@@ -1,29 +1,50 @@
+
+// Counting Coins
+
+// Write a program that will convert a dollar amount to the number of coins that make up the amount.
+// Use the common American coins of quarters, dimes, nickels, and pennies.
+
 /*
   Input: 0.67
   Output:
   {
-    quarters: 3,
+    quarters: 2,
     dimes: 1,
     nickels: 1,
     pennies: 2
   }
-
 */
 
-function coinCounter () {
-    // Initialize a JavaScript object to hold the coins
-    var coinPurse = {
-        quarters: 3,
-        dimes: 1,
-        nickels: 1,
-        pennies: 2
+function coinCounter (x) {
+  // Initialize a JavaScript object to hold the coins
+  let coinPurse = {};
+  
+  coinPurse.quarters = 0;
+  coinPurse.dimes = 0;
+  coinPurse.nickels = 0;
+  coinPurse.pennies = 0;
 
-    };
-    
-    coinPurse.quarters = 4;
-  
-    return coinPurse;
-  }
-  
-  var coins = coinCounter()
-  console.log(coins);
+  let amount = x;
+
+  coinPurse.quarters = Math.floor(amount / .25);
+  amount = amount - (coinPurse.quarters * .25);
+  console.log(amount)
+  coinPurse.dimes = Math.floor(amount / .10);
+  console.log(amount)
+  amount = amount - (coinPurse.dimes * .10);
+  console.log(amount)
+  coinPurse.nickels = Math.floor(amount / .05);
+  console.log(amount)
+  amount = amount - (coinPurse.nickels * .05);
+  console.log(amount)
+  coinPurse.pennies = Math.floor(amount / .01);
+  console.log(amount)
+  amount = amount - (coinPurse.pennies * .01);
+
+  console.log(amount);
+
+  return coinPurse;
+}
+
+let coins = coinCounter(.91)
+console.log(coins);
